8df73b8a5348309a21105b9c7d20f8baa65cf02a
XxVascoDuranxX <vascoduran14@gmail.com>
2026-01-29 23:55:34 +0100

    ultimos retoques
    Paginas arregladas, favicons añadidos, javascript ampliado, css retocado, imagen red bull eliminada por problemas de continuidad de pagina

---
+++ Scripts/Javascript_tienda.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const carrito = document.getElementById("carrito");
+    carrito.addEventListener("click", () => {
+        alert("Esta funcionalidad aún no está disponible");
+    });
+});
